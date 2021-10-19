a491f7e44bf707dd966af190fb4f2289ce11f6c1
epoeana <77653644+epoeana@users.noreply.github.com>
2021-10-18 23:29:03 -0400

    Add files via upload
    update

---
+++ Assignments/Assignment 4/addNameProc.php
@@ -0,0 +1,73 @@
+<?php
+
+class AddNamesProc {
+	// This function will clear names
+	private function clearNamesFunc() {
+		$_POST["clearNames"]= NULL;
+		return NULL;    
+	}
+
+	// This function will add namess to a list
+	private function addNameFunc() {
+
+		// If no name is entered, send an alert to the screen
+		if(trim($_POST["fullName"]) == NULL)
+		{
+			echo '<script language="javascript">';
+			echo 'alert("Invalid Entry")';
+			echo '</script>';
+			return ($_POST["nameList"]);
+		}
+
+		// Seperate the full name with a coma 
+		$newName = explode(" ",trim($_POST["fullName"]));
+		$seperateName = $newName[1] .", " . $newName[0];
+		$sortArray = trim($_POST["nameList"]);
+
+		// Sort the names
+		if(isset($sortArray))
+		{
+			$sortArray .= "\n" .$seperateName;
+			$sortArray = explode("\n",$sortArray);
+			sort($sortArray, SORT_STRING | SORT_FLAG_CASE);
+			$sortArray = implode("\n",$sortArray);
+			$_POST["clearNames"] = NULL;
+			$_POST["addName"]= NULL;
+			return $sortArray;
+		} 
+
+		// If no names in the list, return the list
+		else 
+		{
+			return $seperateName; 
+		}
+	}
+
+	
+	public function formValidateFunc() {
+		$nameList = NULL;
+		// If addName holder is set or clearNames is set then...
+		if(isset($_POST["addName"]) || isset($_POST["clearNames"]))
+		{
+			// add the name in addName list
+			if(isset($_POST["addName"])){
+				$nameList = $this->addNameFunc();
+				$_POST["addName"]= NULL;
+			} 
+			// if clearNames is clicked, clear the names
+			elseif (isset($_POST["clearNames"])){
+				$namelist = $this->clearNamesFunc();
+				$_POST["clearNames"]= NULL;
+			} 
+			// anything else is invalid
+			else{
+				echo "invalid request";
+			}
+
+			return $nameList;
+		}
+	}
+}
+
+
+?>
---
+++ Assignments/Assignment 4/form.php
@@ -0,0 +1,49 @@
+<?php
+
+	require_once 'addNameProc.php'; 
+	$addName = new AddNamesProc(); 
+	$output = $addName->formValidateFunc();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    
+	<title>Add Names Form</title>
+
+</head>
+  
+<body>
+  	<div class="container">  
+		<form action="form.php" method="post">	
+		<h1>Add Names</h1> 
+		<div class="form-group">
+			<input type="submit" class="btn btn-primary" name="addName" id="B1" value="Add Name">
+			<input type="submit" class="btn btn-primary" name="clearNames" id="B2" value="Clear Names">
+		</div>
+		<div class="col-md-12">
+			<label for="fullName" class="form-label">Enter Name</label>
+			<input type="text" class="form-control" id="fullName" name="fullName" alt="Form input for new name">
+		</div>
+		<div class="col-md-12">
+			<label for="nameList" class="form-label">List of Name</label>
+			<textarea style="height: 500px" class="form-control" id="nameList" name="nameList">
+			<ol>
+				<li>
+				<?php echo $output ?>
+				</li>
+			</ol>	
+			</textarea>
+		</div>
+		</form>
+	</div>
+</body>
+</html>
